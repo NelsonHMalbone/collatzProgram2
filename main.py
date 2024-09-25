b46d0a05713467cf6c3f91e13b08c2b46df4797e
Nelson Malbone <nhmalbone@icloud.com>
2024-09-25 18:41:25 -0400

    09/25/2024 we have intergrated a user input to ask user to use Q or C to quit or continue program added additional notes or comments

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # project Collatz Sequence: Practice Project from the book
 # automate the boring stuff chapter 3
 # point to this is to also make a continue section after getting asked to continue or not.
-# import sys
+# spacing could be wrong but it easier for me to see what is what
+import sys
 
 
-def collatz(number):
+def collatz(number): # the function
     if number % 2 == 0: # for even numbers
         results = number // 2
     else:
@@ -13,9 +14,21 @@ def collatz(number):
     return results
 
 while True: # main loop
-    try:
-        number = int(input("Enter a whole interger: ")) # asking user to input a whole number
-        while collatz(number) != 1:
-            number = collatz(number)
-    except ValueError:
-        print("Please only use whole intergers only")
+    user_inputs = input("to quit 'Q' to continue 'C': ") # asking user to quit or continue
+
+    if user_inputs == "C": # has user continue with program
+
+        try:
+            number = int(input("Enter a whole interger: ")) # asking user to input a whole number
+            while collatz(number) != 1:
+                number = collatz(number)
+
+        except ValueError: # so if user does not input a interger it wont error out
+            print("Please only use whole intergers only")
+
+    elif user_inputs == "Q": # has user quit program
+        print("Goodbye")
+        sys.exit()
+
+    else: # if user does not hit C or Q then it will show this message
+        print('Please use uppercase "C" or "Q" to continue program')
