28e5b75c730f6d7a3f8851b3404b1c921c94932c
Nelson Malbone <nhmalbone@icloud.com>
2024-09-25 18:31:46 -0400

    09/25/2024 got rid of then sys exit command it was throwing it off added some comments to what some of the code does

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 # project Collatz Sequence: Practice Project from the book
 # automate the boring stuff chapter 3
 # point to this is to also make a continue section after getting asked to continue or not.
-import sys
+# import sys
 
 
 def collatz(number):
-    if number % 2 == 0:
+    if number % 2 == 0: # for even numbers
         results = number // 2
     else:
-        results = 3 * number + 1
+        results = 3 * number + 1 # for odd numbers
     print(results)
     return results
 
-while True:
+while True: # main loop
     try:
-        number = int(input("Enter a whole interger: "))
+        number = int(input("Enter a whole interger: ")) # asking user to input a whole number
         while collatz(number) != 1:
             number = collatz(number)
-            sys.exit()
     except ValueError:
         print("Please only use whole intergers only")
