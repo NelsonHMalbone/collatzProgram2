bf147424fc2414fa64242a0c2f881eac63be6780
Nelson Malbone <nhmalbone@icloud.com>
2024-09-25 18:19:26 -0400

    09/25/2024 got rid of the two back to back while loops struggling to get the 1 to show up

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 # project Collatz Sequence: Practice Project from the book
 # automate the boring stuff chapter 3
 # point to this is to also make a continue section after getting asked to continue or not.
-
 import sys
-def collatz(num):
-    if num % 2 == 0:  # for the even numbers
-        result = num // 2
-    elif num % 2 == 1: # the odd numbers
-        result = 3 * num + 1
 
-    while result == 1:
-        print(result)
-        sys.exit()
 
-    while result !=1:
-        print(result)
-        num = result
-        return collatz(num)
+def collatz(number):
+    if number % 2 == 0:
+        results = number // 2
+    else:
+        results = 3 * number + 1
+    print(results)
+    return results
 
-print("enter a number: ")
-try:
-    num = int(input())
-    collatz(num)
-except ValueError:
-    print("You must use a Interger number")
+while True:
+    try:
+        number = int(input("Enter a whole interger: "))
+        while collatz(number) != 1:
+            number = collatz(number)
+            sys.exit()
+    except ValueError:
+        print("Please only use whole intergers only")
